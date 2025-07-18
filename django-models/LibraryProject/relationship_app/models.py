1e3678858606b817b182bd8b94e7e92a74db5b0a
Reda Bagha <baghareda@gmail.com>
2025-07-18 03:35:23 +0100

    fix 4

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,37 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-
-class Profile(models.Model):
-    author = models.OneToOneField(Author, on_delete=models.CASCADE)
-    biography = models.TextField()
-
-
-    def __str__(self):
-        return f"Profile of {self.author.name}"
-
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Reader(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -26,3 +26,55 @@ print("Books by George Orwell:", orwell.book_set.all())
 
 # Get librarian info
 print("Librarians:", Librarian.objects.all())
+
+def list_books_in_library(library_name):
+    from relationship_app.models import Library
+
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        for book in books:
+            print(book.title)
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' not found.")
+
+def list_librarians_in_library(library_name):
+    from relationship_app.models import Library
+
+    try:
+        library = Library.objects.get(name=library_name)
+        librarians = library.librarians.all()
+        for librarian in librarians:
+            print(librarian.name)
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' not found.")
+
+def list_libraries_by_author(author_name):
+    from relationship_app.models import Author
+
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        libraries = set()
+
+        for book in books:
+            libraries.add(book.library.name)
+
+        for library in libraries:
+            print(library)
+
+    except Author.DoesNotExist:
+        print(f"Author '{author_name}' not found.")
+
+
+def list_books_in_library(library_name):
+    from relationship_app.models import Library
+
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        for book in books:
+            print(f"{book.title} by {book.author.name}")
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' not found.")
+
