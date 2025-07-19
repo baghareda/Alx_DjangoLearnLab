c3a860c1344197bcf47228f86cc43d75736ab1de
Reda Bagha <baghareda@gmail.com>
2025-07-19 01:47:01 +0100

    Implement last one 3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -25,8 +25,9 @@ class Book(models.Model):
 
     class Meta:
         permissions = [
-            ("can_view_book", "Can view book"),
-            ("can_edit_book", "Can edit book"),
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
         ]
 
 class Librarian(models.Model):
