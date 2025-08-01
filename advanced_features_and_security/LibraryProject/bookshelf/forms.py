e57c283b978e3b5da84325c769f65e5b004a2090
Reda Bagha <baghareda@gmail.com>
2025-08-02 00:11:24 +0100

    um4

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,9 @@
+# bookshelf/forms.py
+
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
