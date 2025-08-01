1c547d8a2389f2cfeb5406c8fe7bc5f0a8da0a45
Reda Bagha <baghareda@gmail.com>
2025-08-02 00:26:57 +0100

    ex0-refix

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -13,3 +16,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    class Meta:
+        managed = False
