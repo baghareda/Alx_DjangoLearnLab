a0971d9d5fea87e063c0153a2ea606d79f277673
Reda Bagha <baghareda@gmail.com>
2025-08-01 18:43:08 +0100

    d3

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,9 +2,19 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')       # show these columns in admin list view
     list_filter = ('publication_year',)                          # filter sidebar by year
-    search_fields = ('title', 'author')                          # enable search by title or author
+    search_fields = ('title', 'author') # enable search by title or author
+    
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.contrib.auth.admin import UserAdmin
 # Create your models here.
 
 class Book(models.Model):
@@ -22,12 +21,3 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         pass
     
-    
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
