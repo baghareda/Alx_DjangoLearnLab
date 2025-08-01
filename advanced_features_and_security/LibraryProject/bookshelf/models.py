694dc5fe7b971f400cea0504787ac44c6730c633
Reda Bagha <baghareda@gmail.com>
2025-08-02 00:30:32 +0100

    ex0-refix-2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -33,3 +33,8 @@ class CustomUser(AbstractUser):
 
     class Meta:
         managed = False  # This prevents migrations and DB table creation
+        
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
