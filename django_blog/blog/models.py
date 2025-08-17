c36388ad29eee1dac558d7ac271d5bd5254c1f7d
Reda Bagha <baghareda@gmail.com>
2025-08-17 17:24:01 +0100

    comment section done

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,3 +23,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile({self.user.username})"
+    
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
